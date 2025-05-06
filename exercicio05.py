f6314e3be7f29fab9ba0ddc1cd20bebb381312e5
Matheus Buje Pegorin <00001111117160sp@aluno.educacao.sp.gov.br>
2025-05-06 12:12:31 -0300

    fizemos o exercicio 5

--- exercicio05.py
+++ exercicio05.py
@@ -1,10 +1,13 @@
-def adicionar_aluno(nome, email, serie):
+def adicionar_aluno(nome, email, serie, nota1, nota2, nota3):
     lista_alunos = []
     aluno = {
         'nome': nome,
         'email': email,
-        'serie': serie
+        'serie': serie,
+        "lista_notas": [nota1, nota2, nota3]
     }
 
     lista_alunos.append(aluno)
     return lista_alunos
+
+print(adicionar_aluno("matheus", "matheus@g.com", "2 TB", nota1, nota2, nota3))
