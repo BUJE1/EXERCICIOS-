896e924d032da860aef3bf6c88906ef5be821c90
Matheus Buje Pegorin <00001111117160sp@aluno.educacao.sp.gov.br>
2025-05-12 14:18:12 -0300

    importamos o exercico 4 para o 5

--- exercicio05.py
+++ exercicio05.py
@@ -1,13 +1,18 @@
-def adicionar_aluno(nome, email, serie, nota1, nota2, nota3):
+from exercicio04 import somar
+
+def adicionar_aluno(nome, email, serie, nota1=0, nota2=0, nota3=0):
     lista_alunos = []
+    notas = [nota1, nota2, nota3]
     aluno = {
         'nome': nome,
         'email': email,
         'serie': serie,
-        "lista_notas": [nota1, nota2, nota3]
+        "lista_notas": [nota1, nota2, nota3],
+        'media': somar([nota1, nota2, nota3])
     }
 
     lista_alunos.append(aluno)
+    media = somar(aluno["lista_notas"])
     return lista_alunos
 
-print(adicionar_aluno("matheus", "matheus@g.com", "2 TB", nota1, nota2, nota3))
+print(adicionar_aluno("matheus", "matheus@g.com", "2 TB", 9, 8, 2))
